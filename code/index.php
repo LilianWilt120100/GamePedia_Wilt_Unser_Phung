377a96c7e0cd3f801f652ec3f31afc2ca42c0a7c
KevinUnser <unserkevinpro@gmail.com>
2021-03-29 21:22:31 +0200

    Modification tp1

--- code/index.php
+++ code/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Illuminate\Database\Capsule\Manager as DB;
 
 use games\models\Character;
+use games\models\Company;
 
 $db = new DB();
 $config = parse_ini_file(__DIR__.'/src/conf/db.config.ini');
